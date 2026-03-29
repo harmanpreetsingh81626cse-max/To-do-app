cdb192842f21fb6e67ee63d691d08f335e2109ad
Harman <harman.preet.singh81626.cse@gamil.com>
2026-03-30 00:08:46 +0530

    css and rendring added

--- js/script.js
+++ js/script.js
@@ -6,16 +6,49 @@ const uncomplete = document.getElementById("uncompleted");
 const tasklist = document.getElementById("tasklist");
 const add = document.getElementById("add-task");
 
-add.addEventListener("click",()=>{
-    if (task.value != ""){
-        const li = document.createElement("li");   
-        const btn = document.createElement("button");
-        btn.textContent = "completed";
-        li.textContent = task.value;
-        tasklist.appendChild(li);
-        li.appendChild(btn);
-        task.value = "";
+add.addEventListener("click",addtask);
+
+function addtask(){
+    if (task.value === "") return;
+    const task_position = {
+        completed : false,
+        text :task.value
+
     }
-});
+
+    t.push(task_position);
+    rendertask();
+    task.value = "";
+
+}
+
+function createtask(task){
+   
+    const li = document.createElement("li");
+    li.textContent = task;
+    const completebtn = document.createElement("button");
+    const deletebtn = document.createElement("button");
+    completebtn.textContent = "Complete";
+    deletebtn.textContent = "Delete";
+
+    li.appendChild(completebtn);
+
+    li.appendChild(deletebtn);
+
+    return li;
+   
+}
+
+
+function rendertask(){
+    tasklist.innerHTML = "";
+    t.forEach(task =>{
+        const li = createtask(task);
+        tasklist.appendChild(li);
+    })
+}
+
+
+
 
 
